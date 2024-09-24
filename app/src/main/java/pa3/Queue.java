caea16bb87cdc60cec3261deab63f4c4df0be487
PoliticianSeal <politicianseal@gmail.com>
2024-09-24 19:47:44 -0400

    Assignment with the tests

--- app/src/main/java/pa3/Queue.java
+++ app/src/main/java/pa3/Queue.java
@@ -1,52 +1,57 @@
-
 package pa3;
 
-/**
- * A queue of nodes.
- */
 public class Queue {
+    public Node[] arr;
+    public int track;
+    public int cap;
 
-    /** 
-     * Constructs an empty queue.
-     */
     public Queue() {
-
-        
+        this.track = 0;
+        this.cap = 10;
+        this.arr = new Node[cap];
     }
 
-    /**
-     * Adds a node to the queue.
-     * @param node
-     */
-    public void enqueue(Node node) {
-
-        
+    public void enqueue(Node value) {
+        if (track < cap) {
+            this.arr[track] = value;
+            track += 1;
+        } else {
+            cap += 5;
+            Node[] history = this.arr;
+            this.arr = new Node[cap];
+            for (int i = 0; i < track; i++) {
+                this.arr[i] = history[i];
+            }
+            this.arr[track] = value;
+            track += 1;
+        }
     }
 
-    /**
-     * Removes and returns the node at the front of the queue.
-     * @return the node at the front of the queue.
-     */
     public Node dequeue() {
-
-        
+        if (isEmpty()) {
+            System.out.println("Empty queue");
+            return null;
+        }
+        Node removed = this.arr[0];
+        for (int i = 0; i < track - 1; i++) {
+            this.arr[i] = this.arr[i + 1];
+        }
+        track -= 1;
+        return removed;
     }
 
-    /**
-     * Returns true if the queue is empty.
-     * @return true if the queue is empty.
-     */
     public boolean isEmpty() {
-        
+        return track == 0;
     }
 
     public static void main(String[] args) {
         Queue queue = new Queue();
         Node node1 = new Node(4);
-        Node node2 = new Node(4);
+        Node node2 = new Node(5);
         queue.enqueue(node1);
         queue.enqueue(node2);
-        System.out.println(queue.dequeue().value); // Should print 4
+        System.out.println(queue.dequeue().value);
+        System.out.println(queue.dequeue().value);
+        System.out.println(queue.dequeue());
     }
-    
 }
